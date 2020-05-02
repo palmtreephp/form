cce3b87be2538fad9d9631449ecae0d1bffad378
andyexeter <palmer.andy@gmail.com>
2020-05-02 11:59:40 +0100

    Fix ajax in examples

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -43,6 +43,6 @@ HTML;
 function get_scripts()
 {
     return <<<HTML
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
 HTML;
 }
