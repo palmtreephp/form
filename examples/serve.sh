df2d26f9d23d2951b077ac4b5540147bf6f5d162
andyexeter <palmer.andy@gmail.com>
2021-03-22 18:07:07 +0000

    Use basic php image for serving examples

--- examples/serve.sh
+++ examples/serve.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-docker build ./examples -t palmtree-form-examples && docker run --rm \
+port=${1:-8080}
+
+docker run --rm \
   -v $PWD/:/app -v $PWD/public:/app/examples/public \
-  -p 8080:8080 \
-  palmtree-form-examples \
-  php -S 0.0.0.0:8080 -t /app/examples $@ 2>&1 | sed 's/0.0.0.0/127.0.0.1/'
+  -p $port:$port \
+  php:7.4-cli-alpine \
+  php -S 0.0.0.0:$port -t /app/examples 2>&1 | sed 's/0.0.0.0/127.0.0.1/'
