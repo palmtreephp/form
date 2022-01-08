02c4359cc2137aa60d30148cfddb82994a364fea
andyexeter <palmer.andy@gmail.com>
2022-01-08 11:53:28 +0000

    Update examples to use Bootstrap 5.1

--- examples/.bootstrap.php
+++ examples/.bootstrap.php
@@ -36,7 +36,7 @@ function send_json_error($data = [])
 function get_styles()
 {
     return <<<HTML
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 HTML;
 }
 
