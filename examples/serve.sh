40764537c12badae00c3d75aded0affc2a24ef55
andyexeter <palmer.andy@gmail.com>
2023-05-01 12:46:02 +0100

    Update php image used in examples to allow xdebug

--- examples/serve.sh
+++ examples/serve.sh
@@ -3,7 +3,7 @@
 port=${1:-8080}
 
 docker run --rm \
-  -v $PWD/:/app -v $PWD/public:/app/examples/public \
-  -p $port:$port \
-  php:7.4-cli-alpine \
-  php -S 0.0.0.0:$port -t /app/examples 2>&1 | sed 's/0.0.0.0/127.0.0.1/'
+  -v "$PWD"/:/app -v "$PWD/public":/app/examples/public \
+  -p "$port":"$port" \
+  xisgo/php81-cli-xdebug:1.0.0 \
+  php -c /app/examples/php.ini -S "0.0.0.0:$port" -t /app/examples 2>&1 | sed 's/0.0.0.0/127.0.0.1/'
