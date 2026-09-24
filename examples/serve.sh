f02ef87516b5b4a7da371f930b6aafa3ce8ff099
andyexeter <palmer.andy@gmail.com>
2026-09-24 12:21:06 +0100

    Update examples docker image to php 8.5

--- examples/serve.sh
+++ examples/serve.sh
@@ -5,5 +5,5 @@ port=${1:-8080}
 docker run --rm \
   -v "$PWD"/:/app -v "$PWD/dist":/app/examples/dist \
   -p "$port":"$port" \
-  xisgo/php-cli-xdebug:8.3 \
+  php:8.5-cli \
   php -c /app/examples/php.ini -S "0.0.0.0:$port" -t /app/examples 2>&1 | sed 's/0.0.0.0/localhost/'
